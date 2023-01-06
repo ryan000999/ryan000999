c070e122040e4f7569c316bbf7a9237734b221d5
ryan000999 <thanhson07059@gmail.com>
2023-01-06 11:05:24 -0500

    day 1 - new

--- main.py
+++ main.py
@@ -1,2 +1,20 @@
 print("❤️💚") # Ctrl + Alt + N
 # Open terminal = Ctrl + J
+
+with open("data.txt", 'w', encoding="utf8") as file:
+    print("❤️💚💙", file=file)
+
+#------------------------------------------------------
+# variable - Ctrl + /
+"""
+line 1
+line 2
+line 3
+line 4
+line 5
+"""
+_ = 23
+print(_)
+
+# type of value
+print(type(0j)) # a + bi i^2 = -1
