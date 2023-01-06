62e2f2ae44514157632ae2b365c58774d3e8a988
ryan000999 <thanhson07059@gmail.com>
2023-01-06 11:27:00 -0500

    day 1 - update

--- main.py
+++ main.py
@@ -18,3 +18,50 @@ print(_)
 
 # type of value
 print(type(0j)) # a + bi i^2 = -1
+
+# Declare a constant
+PI = 3.14159265
+a = 300 # [-5, 256]
+
+# full_name. age. is_over_18, user_name, ....
+print(...)
+
+#abcxyz - DO NOT
+# Operators
+"""
++, -, *, /, //, **, %
+>, <, >=, <=, ==, != => bool (True/False)
+not > and > or
+falsy: 0, 0.0, 0j, None, '', [], set(), (), {}, Fraction(0, 1)
+expr1 and|or expr2
+if expr1 => bool(expr1) = True then and => expr2 else expr1
+if expr1 => bool(expr1) = True then or  => expr1 else expr2
+not => bool (True/False)
+"""
+# Ctrl + X
+print(4 / 3) # / => float
+print(4 // 3) # // => int
+print(.5 > 1) # False
+
+print('a' > 'b') # ord('a') > ord('b') <=> 97 > 98 = False
+
+print(1 and 2)
+print(0 and 2)
+
+print('' or None)
+print('Anna' or 'Beta')
+
+print(not '') # True
+print(not 1) # False
+
+# f-strings, format(), str(), float(), int(), bool()
+age = 34
+
+print(f"I am {age}") # Shift + Alt + Down Arrow
+print("I am {}".format(age)) # Shift + Alt + Down Arrow
+print("I am " + str(age))
+
+print(float(age))
+
+print(int(True)) # 1
+print(int(False)) # 0
